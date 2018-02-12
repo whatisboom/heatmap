11da0e8c6df92536c2a26d0b39dcb17faef4fa2f
Brandon Jernigan <bjernigan@netspend.com>
2018-02-12 17:47:47 -0600

    stubbed out google maps component

--- src/components/heatmap.jsx
+++ src/components/heatmap.jsx
@@ -1,5 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import {
+  GoogleMap,
+  HeatmapLayer,
+  Marker,
+  withGoogleMap,
+  withScriptjs
+} from 'react-google-maps';
+
+const MapComponent = withScriptjs(withGoogleMap((props) => {
+  <GoogleMap
+    defaultZoom={4}
+    defaultCenter={{
+      lat: 30.2672,
+      lng: -97.7431
+    }}>
+    <Marker position={{
+      lat: 30.2672,
+      lng: -97.7431
+    }} />
+  </GoogleMap>
+}));
 
 export class Heatmap extends Component {
   render() {
@@ -7,10 +28,15 @@ export class Heatmap extends Component {
       data
     } = this.props;
 
-    const rows = data.map((item, i) => {
-      return <p key={i}>Longitude: {item.lng}, Latitude: {item.lat}</p>
-    })
-    return rows;
+
+    return (
+      <MapComponent
+        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `400px` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+        />
+    );
   }
 }
 
